7c54938255f6bd18ac50dd401b301c6609f13308
irakozemarie <irakoze.antonny@gmail.com>
2024-04-13 14:03:05 +0200

    new features

--- src/components/Countries.jsx
+++ src/components/Countries.jsx
@@ -43,7 +43,7 @@ const Countries = () => {
   return (
     <CountriesSection>
       {/* Top part  */}
-      <div id='top-section'>
+      <div id="top-section">
         <div>
           <h3>View Countries</h3>
           <p>Page 1 of 5</p>
@@ -54,22 +54,28 @@ const Countries = () => {
       </div>
 
       {/* List of countries */}
-      <div id='countries' className='flex flex-wrap w-full justify-between md:gap-1'>
-        {listOfCountries.length > 0 && listOfCountries.map((country, index) => {
-          return (
-            <div key={index} className='w-5/12 md:w-1/5 mb-5'>
-              <img src={country.flags.svg} alt={country.flags.alt} />
-              <p>{country.name.common}</p>
-              <p>{country.capital}</p>
-              <p>{country.population}</p>
-              <p>{country.continents}</p>
-            </div>
-          )
-        })}
+      <div
+        id="countries"
+        className="flex flex-wrap w-full justify-between md:gap-1"
+      >
+        {listOfCountries.length > 0 &&
+          listOfCountries.map((country, index) => {
+            return (
+              <div key={index} className="w-5/12 md:w-1/5 mb-5">
+                <img src={country.flags.svg} alt={country.flags.alt} />
+                <p>{country.name.common}</p>
+                <p>{country.capital}</p>
+                <p>{country.population}</p>
+                <p>{country.continents}</p>
+              </div>
+            );
+          })}
 
         {loading && <p>Loading...</p>}
 
-        {(!loading && listOfCountries.length === 0) && <p>No countries available</p>}
+        {!loading && listOfCountries.length === 0 && (
+          <p>No countries available</p>
+        )}
 
         {/* Using Ternary operators */}
         {/* {listOfCountries.length > 0
@@ -84,19 +90,34 @@ const Countries = () => {
             })
           : <p>No countries available</p>
         } */}
-
       </div>
 
       {/* Pagination  */}
-      <div id='pagination'>
-        <button type="button">1</button>
-        <button type="button">2</button>
-        <button type="button">3</button>
-        <button type="button">4</button>
-        <button type="button">5</button>
+      <div id="pagination">
+        <button class="bg-blue-500 hover:bg-blue-400 text-white font-bold py-2 px-4 border-b-4 border-blue-700 hover:border-blue-500 rounded gap-3">
+          1
+        </button>
+        <button class="bg-blue-500 hover:bg-blue-400 text-white font-bold py-2 px-4 border-b-4 border-blue-700 hover:border-blue-500 rounded gap-3">
+          2
+        </button>
+        <button class="bg-blue-500 hover:bg-blue-400 text-white font-bold py-2 px-4 border-b-4 border-blue-700 hover:border-blue-500 rounded">
+          3
+        </button>
+        <button class="bg-blue-500 hover:bg-blue-400 text-white font-bold py-2 px-4 border-b-4 border-blue-700 hover:border-blue-500 rounded">
+          4
+        </button>
+        <button class="bg-blue-500 hover:bg-blue-400 text-white font-bold py-2 px-4 border-b-4 border-blue-700 hover:border-blue-500 rounded">
+          5
+        </button>
+        <button class="bg-blue-500 hover:bg-blue-400 text-white font-bold py-2 px-4 border-b-4 border-blue-700 hover:border-blue-500 rounded">
+          6
+        </button>
+        <button class="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded">
+  Button
+</button>
       </div>
     </CountriesSection>
-  )
+  );
 }
 
 export default Countries
